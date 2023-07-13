da2148dc739934dd4a516878901e42cef7000eb4
MorabaTech <50992477+MorabaTech@users.noreply.github.com>
2023-07-13 14:41:22 +0200

    major changes

--- lip/app/Http/Controllers/HomeController.php
+++ lip/app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
     public function index()
     {
 
+
         return response()->json([
             'message' => 'You are logged in leeroy!',
             'user' => auth()->user()
